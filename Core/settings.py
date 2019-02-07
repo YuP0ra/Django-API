7b9f9173ca14aa3e6274ebf609afd8ff2f3cd166
AbdElAziz Moftah <vb.promax@gmail.com>
2019-02-08 00:08:05 +0200

    amazon localhost added too, debug true

--- Core/settings.py
+++ Core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3otfuenp_+b_lr!g9vuvg8x9r2-^wya_&g+7g#6eaw262rna4t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '35.158.163.26']
 
 
 # Application definition
